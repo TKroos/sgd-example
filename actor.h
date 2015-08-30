2ceaa6c8bb6af7b3032e5b5234f845eeb3580136
Andrew Grapsas <aagrapsas@gmail.com>
2015-08-30 15:23:24 -0400

    Remove tabs from headers

--- actor.h
+++ actor.h
@@ -11,33 +11,33 @@ namespace sgde
 class Actor
 {
   private:
-	Weapon* d_weapon;
-	  // Actor's individual weapon in the world. Not const
-	  // because a weapon can have ammunition or some such
-	  // thing that is modified by the actor class when
-	  // it attacks.
+        Weapon* d_weapon;
+          // Actor's individual weapon in the world. Not const
+          // because a weapon can have ammunition or some such
+          // thing that is modified by the actor class when
+          // it attacks.
 
   public:
-	// CONSTRUCTORS
-	Actor();
-	  // Default constructor.
-	Actor( Weapon* weapon );
-	  // Provides a weapon reference to an actor.
+        // CONSTRUCTORS
+        Actor();
+          // Default constructor.
+        Actor( Weapon* weapon );
+          // Provides a weapon reference to an actor.
 
-	// ACCESSORS
-	Weapon* weapon() const;
-	  // Retrieves the actor's current weapon.
-	
-	// MUTATORS
-	void setWeapon( Weapon* weapon );
-	  // Sets the actor's current weapon.
+        // ACCESSORS
+        Weapon* weapon() const;
+          // Retrieves the actor's current weapon.
+        
+        // MUTATORS
+        void setWeapon( Weapon* weapon );
+          // Sets the actor's current weapon.
 };
 
 // FREE OPERATORS
 inline
 std::ostream& operator<<( std::ostream& stream, const Actor& actor )
 {
-	return stream << "{ weapon : " << *( actor.weapon() ) << " }";
+        return stream << "{ weapon : " << *( actor.weapon() ) << " }";
 }
 
 // CONSTRUCTORS
@@ -55,14 +55,14 @@ Actor::Actor( Weapon* weapon ) : d_weapon( weapon )
 inline
 Weapon* Actor::weapon() const
 {
-	return d_weapon;
+        return d_weapon;
 }
 
 // MUTATORS
 inline
 void Actor::setWeapon( Weapon* weapon )
 {
-	d_weapon = weapon;
+        d_weapon = weapon;
 }
 
 } // End sgde namespace
